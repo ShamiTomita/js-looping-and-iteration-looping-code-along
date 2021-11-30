0ffc1912e8bfa9d1174e1cf23b7718931653939a
Shami <shamitomita@gmail.com>
2021-11-29 21:43:08 -0500

    commit tree 2234dc47a94e050028e2c6cc29e8cdd8f2e3c3e3

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solutions in this file
+let eventName = "birthday"
+let messages = []
+function writeCards(names, eventName){
+  for (let i = 0; i < names.length; i++){
+    console.log(`Thank you, ${names[i]}, for the wonderful ${eventName} gift!`)
+    messages.push(`Thank you, ${names[i]}, for the wonderful ${eventName} gift!`)
+  }
+  return messages;
+}
+
+writeCards(names, eventName);
+
+
+function countDown(number){
+  while (number > 0){
+    console.log(number);
+    number -= 1;
+  }
+  console.log(number);
+}
